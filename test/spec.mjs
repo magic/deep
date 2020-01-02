bb33d8792abc77385e7eb2ac3ea80fc4a962054a
Jascha <jascha@jaeh.at>
2020-01-02 19:59:09 +0100

    update tests

--- test/spec.mjs
+++ test/spec.mjs
@@ -1,10 +1,11 @@
 import { is } from '@magic/test'
 
-import * as deep from '../src/index.mjs'
+import d, * as deep from '../src/index.mjs'
 
 const funcs = [
   deep.equal,
   deep.equals,
+  deep.eq,
   deep.different,
   deep.merge,
   deep.diff,
@@ -14,10 +15,16 @@ const funcs = [
 ]
 
 const fns = [
-  { fn: () => funcs.every(is.function), expect: true },
-  { fn: () => deep.equal.toString() === deep.equals.toString() },
-  { fn: () => deep.different.toString() === deep.diff.toString() },
-  { fn: () => Object.keys(deep), expect: is.length.equal(funcs) },
+  { fn: () => d, expect: is.deep.eq(deep.default), info: 'default exports match' },
+  { fn: () => funcs.every(is.function), expect: true, info: 'all exports are functions' },
+  { fn: () => deep.equal, expect: is.deep.eq(deep.equals), info: 'deep.equals aliases deep.equal' },
+  { fn: () => deep.equal, expect: is.deep.eq(deep.eq), info: 'deep.eq aliases deep.equal' },
+  {
+    fn: () => deep.different,
+    expect: is.deep.eq(deep.diff),
+    info: 'deep.diff aliases deep.different',
+  },
+  { fn: () => Object.keys(deep), expect: is.length.equal(funcs), info: 'expected length matches' },
 ]
 
 export default fns
