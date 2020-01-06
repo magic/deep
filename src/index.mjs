e870af0a877af9f0c82aa83ff4d7b34b19cd9206
Jascha <jascha@jaeh.at>
2020-01-06 11:58:38 +0100

    use equal and different from @magic/types

--- src/index.mjs
+++ src/index.mjs
@@ -1,15 +1,14 @@
-import { equal as equ } from './equal.mjs'
-import { different as di } from './different.mjs'
+import is from '@magic/types'
 
 import { loop as lo } from './loop.mjs'
 import { merge as me } from './merge.mjs'
 import { flatten as fl } from './flatten.mjs'
 
-export const equal = equ
-export const equals = equ
-export const eq = equ
-export const different = di
-export const diff = di
+export const equal = is.deep.equal
+export const equals = is.deep.equal
+export const eq = is.deep.equal
+export const different = is.deep.different
+export const diff = is.deep.different
 
 export const flatten = fl
 export const loop = lo
