3966299882ee92b20e61bbd00d320eb91a24ebfe
jascha <j@jaeh.at>
2021-12-24 16:23:25 +0100

    test/spec now uses @magic/test.version

--- test/spec.mjs
+++ test/spec.mjs
@@ -1,30 +1,16 @@
-import { is } from '@magic/test'
+import { version } from '@magic/test'
 
-import d, * as deep from '../src/index.mjs'
+import d from '../src/index.mjs'
 
-const funcs = [
-  deep.equal,
-  deep.equals,
-  deep.eq,
-  deep.different,
-  deep.merge,
-  deep.diff,
-  deep.loop,
-  deep.flatten,
-  () => 'mockDefaultExport',
-]
+const spec = {
+  equal: 'fn',
+  equals: 'fn',
+  eq: 'fn',
+  different: 'fn',
+  diff: 'fn',
+  flatten: 'fn',
+  loop: 'fn',
+  merge: 'fn',
+}
 
-const fns = [
-  { fn: () => d, expect: is.deep.eq(deep.default), info: 'default exports match' },
-  { fn: () => funcs.every(is.function), expect: true, info: 'all exports are functions' },
-  { fn: () => deep.equal, expect: is.deep.eq(deep.equals), info: 'deep.equals aliases deep.equal' },
-  { fn: () => deep.equal, expect: is.deep.eq(deep.eq), info: 'deep.eq aliases deep.equal' },
-  {
-    fn: () => deep.different,
-    expect: is.deep.eq(deep.diff),
-    info: 'deep.diff aliases deep.different',
-  },
-  { fn: () => Object.keys(deep), expect: is.length.equal(funcs), info: 'expected length matches' },
-]
-
-export default fns
+export default version(d, spec)
